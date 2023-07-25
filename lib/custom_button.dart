d802bf9c16351fe298f3199d0bf9efd851895f65
lakshydeep-14 <lvikramsah@gmail.com>
2023-07-25 10:14:48 +0545

    api call

--- lib/custom_button.dart
+++ lib/custom_button.dart
@@ -1,19 +1,38 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget {
+String status = 'Initial';
+
+class CustomButton extends StatefulWidget {
   final String text;
-  final VoidCallback onPressed;
+  final String email;
+  final String password;
 
   const CustomButton({
-    required this.text,
-    required this.onPressed,
+    required this.text, required this.email, required this.password,
   });
 
   @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(text),
+      onPressed: () async {
+        setState(() {
+          status = 'loading...';
+        });
+        var res=await Dio().get('https://dev.eshikshya.com.np/api/user/login',data: {
+          'email':widget.email,
+          'password':widget.password
+        });
+        if(res['']){setState(() {
+          status = 'Loaded';
+        });}
+      },
+      child: Text(widget.text),
     );
   }
 }
